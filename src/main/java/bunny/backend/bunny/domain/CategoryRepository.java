b8d5a259f7a04c44b3b0049577503ef4b4268325
bangyewon <90364660+bangyewon@users.noreply.github.com>
2024-11-05 15:49:33 +0900

    [refactor] 항목설정시 유저당 기타항목 자동생성 수정

--- src/main/java/bunny/backend/bunny/domain/CategoryRepository.java
+++ src/main/java/bunny/backend/bunny/domain/CategoryRepository.java
@@ -1,11 +1,15 @@
 package bunny.backend.bunny.domain;
 
+import bunny.backend.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category,Long> {
     List<Category> findByMemberId(Long memberId);
+
+    Optional<Category> findByMemberAndCategoryName(Member member, String categoryName);
 }
